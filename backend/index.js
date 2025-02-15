ffa30eef7df076ff3cc64b4515b51bf2a85267f9
Mohammad Saif <mohammadsaif0847@gmail.com>
2025-02-15 14:28:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ dbConnect()
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })

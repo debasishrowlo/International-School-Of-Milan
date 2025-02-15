a8b3e79d9a36f68722e90255c59ee4fa6be6b684
Mohammad Saif <mohammadsaif0847@gmail.com>
2025-02-15 14:38:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ dbConnect()
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://international-school-of-milan.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
